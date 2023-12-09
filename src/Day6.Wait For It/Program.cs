b132be1815e22f4cf80be46bccd6cf2e348a3b32
Rafał <rafaczk@gmail.com>
2023-12-09 21:00:42 +0100

    Day p5 pt1

---
+++ src/Day6.Wait For It/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Day6.Wait_For_It
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Part1();
+		}
+
+		public static void Part1()
+		{
+			var lines = File.ReadAllLines("input.txt");
+
+			var times = lines[0].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+				.Skip(1)
+				.Select(int.Parse)
+				.ToArray();
+
+			var distances = lines[1].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+				.Skip(1)
+				.Select(int.Parse)
+				.ToArray();
+
+			int result = 1;
+			int count = 0;
+
+			for (int i = 0; i < distances.Length; i++)
+			{
+				count = 0;
+
+				for (int j = 0; j < times[i]; j++)
+				{
+					if ((times[i] - j) * j > distances[i])
+					{
+						count++;
+					}
+				}
+
+				result *= count;
+			}
+
+			Console.WriteLine(result);
+		}
+	}
+}
