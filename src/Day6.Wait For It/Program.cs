d2cd349aa8ab07f809c2e497967cf335a164b51f
Rafał <rafaczk@gmail.com>
2023-12-14 11:38:54 +0100

    Day6 pt2

--- src/Day6.Wait For It/Program.cs
+++ src/Day6.Wait For It/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Day6.Wait_For_It
+﻿using System.Numerics;
+
+namespace Day6.Wait_For_It
 {
 	public class Program
 	{
 		static void Main(string[] args)
 		{
 			Part1();
+			Part2();
+		}
+
+		public static void Part2()
+		{
+			var lines = File.ReadAllLines("input.txt");
+
+			var timeString = new string(lines[0].Split(':')[1].Where(c => !char.IsWhiteSpace(c)).ToArray());
+			var distanceString = new string(lines[1].Split(':')[1].Where(c => !char.IsWhiteSpace(c)).ToArray());
+
+			var time = BigInteger.Parse(timeString);
+			var distance = BigInteger.Parse(distanceString);
+
+			for (BigInteger i = 1; i < distance; i++)
+			{
+				if (i * (time - i) > distance)
+				{
+					Console.WriteLine(time - i - i + 1);
+					break;
+				}
+			}
+
 		}
 
 		public static void Part1()
